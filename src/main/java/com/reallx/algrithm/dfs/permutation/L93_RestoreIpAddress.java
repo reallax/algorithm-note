4210f52df50f13bd9d88fbbd1f10cc9f0139effe
hong.liu <hong.liu@dmall.com>
2020-09-20 11:08:10 +0800

    l93 commit

--- src/main/java/com/reallx/algrithm/dfs/permutation/L93_RestoreIpAddress.java
+++ src/main/java/com/reallx/algrithm/dfs/permutation/L93_RestoreIpAddress.java
@@ -15,49 +15,98 @@ import java.util.List;
 public class L93_RestoreIpAddress {
 
     public static void main(String[] args) {
-        String s = "25525511135";
+//        String s = "25525511135";
+//        String s = "1111";
+        String s = "010010";
 
         L93_RestoreIpAddress l93 = new L93_RestoreIpAddress();
-        List<List<Integer>> result = l93.restoreIpAddresses(s);
+        List<String> result = l93.restoreIpAddresses(s);
 
-        Main.printList(result);
+        result.stream().forEach(System.out::println);
     }
 
-    public List<List<Integer>> restoreIpAddresses(String s) {
+    public List<String> restoreIpAddresses(String s) {
         int len = s.length();
         int limit = 3;
 
-        List<List<Integer>> result = new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        // 此范围之外的，不能复原成一个IPV4
+        if (len < 4 || len > 12) {
+            return result;
+        }
+
         Deque<Integer> path = new ArrayDeque<>();
 
-        dfs(s, len, limit, 1, path, result);
+        dfs(s, len, limit, 1,0, path, result);
 
         return result;
     }
 
-    private void dfs(String s, int len, int limit, int searchIdx, Deque<Integer> path, List<List<Integer>> result) {
-        if (searchIdx == limit + 1) {
-            result.add(new ArrayList<>(path));
+    private void dfs(String s, int len, int limit,
+                     int searchStart, int searchDepth,
+                     Deque<Integer> path, List<String> result) {
+        if (searchDepth == limit) {
+            result.add(restoreIp(s, new ArrayList<>(path)));
+            System.out.println(String.format("~ [searchIdx: %d ], 添加Path：%s", searchDepth, path));
             return;
         }
 
-        for (int i=searchIdx; i<len; i++) {
-            int subStart = path.size() == 0 ? 0 : path.peekLast();
-            if (i - searchIdx > 3) {
+        for (int i=searchStart; i<=len; i++) {
+            int lastStart = path.size() == 0 ? 0 : path.peekLast();
+            if (i - lastStart > 3) {
+                break;
+            }
+            // len - i 而不是 len -lastStart，是因为我们只搜索三个数，此时i还没有被压入path，lastStart是第二位
+            // 我们应该计算第3位到最后的子串的长度
+            if (searchDepth == limit - 1 && len - i > 3) {
+                continue;
+            }
+            // 如果只有四位，第三个子位之后还应该留有子串
+            if (i == len) {
                 continue;
             }
 
-            String sub = s.substring(subStart, i);
-            int val = Integer.valueOf(sub);
-            if (val > 255) {
+            String sub = s.substring(lastStart, i);
+            if (!checkSegment(sub)) {
                 continue;
+            }
+
+            // 因为只搜寻3个.，所以得出前三个点后，第四个段的ip子段的合法性也要校验一下。需要优化
+            if (searchDepth == limit - 1) {
+                sub = s.substring(i);
+                if (!checkSegment(sub)) {
+                    continue;
+                }
             }
 
             path.addLast(i);
+            System.out.println(String.format("+ [i: %d, searchStart: %d, searchDepth: %d ], 递归前：%s", i, searchStart, searchDepth, path));
 
-            dfs(s, len, limit, searchIdx + 1, path, result);
+            dfs(s, len, limit,  i+ 1, searchDepth + 1, path, result);
 
             path.pollLast();
+            System.out.println(String.format("- [i: %d, searchStart: %d, searchDepth: %d ], 递归后：%s", i, searchStart, searchDepth, path));
         }
+    }
+
+    private boolean checkSegment(String segment) {
+        if (segment.length() > 1 && segment.startsWith("0")) {
+            return false;
+        }
+        int val = Integer.valueOf(segment);
+        if (val > 255) {
+            return false;
+        }
+        return true;
+    }
+
+    private String restoreIp(String s, List<Integer> segs) {
+        StringBuilder b = new StringBuilder();
+        for (int i=0; i<segs.size(); i++) {
+            int lastStart = i == 0 ? 0 : segs.get(i - 1);
+            b.append(s.substring(lastStart, segs.get(i))).append(".");
+        }
+        b.append(s.substring(segs.get(segs.size() - 1), s.length()));
+        return b.toString();
     }
 }
